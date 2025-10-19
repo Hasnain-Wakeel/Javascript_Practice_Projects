8028e4aabad303c4d3b0660a1e32579cfbbe1e54
Hasnain-Wakeel <wakeelanjum112@gmail.com>
2025-10-19 23:28:37 +0500

    Username showing program created

---
+++ showing-username/index.js
@@ -0,0 +1,15 @@
+// Taking Input from User :
+
+// -------------- Simple Way : ------------
+
+// const userName = prompt("Enter Your Name : ");
+// console.log(userName);
+
+// -------------- Professional Way : ------------
+
+let userName;
+document.getElementById("myButton").onclick = function () {
+  userName = document.getElementById("inputField").value;
+  document.getElementById("h3").textContent = `Hello, ${userName}! `;
+  console.log(userName);
+};
